e3301042448b52a526088b5cfc41298a799d6015
fazza7777 <aungbarlaay7777@gmail.com>
2022-12-12 16:38:35 +0630

    passport install and register process

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -2,10 +2,33 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use App\Helpers\ResponseHelper;
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    //
+    // Register
+    public function register(Request $request)
+    {
+        // validate user
+        $request->validate(
+            [
+                'name' => 'required|string|max:100',
+                'email' => 'required|email|unique:users,email',
+                'password' => 'required|min:8|max:20'
+            ]
+        );
+        // register user
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->save();
+        // generate token with passport
+        $token = $user->createToken('Blog')->accessToken;
+        return ResponseHelper::success(['access_token' => $token]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Helpers\ResponseHelper;
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('test',function(){
-  return ResponseHelper::fail('user not found');
-});
+Route::post('/register',[AuthController::class,'register']);
